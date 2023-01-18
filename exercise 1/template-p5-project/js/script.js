ea1121fbdc657c94e27f558cdab2812131b4db3d
Wutangpatrol <Wutangpatrol@users.noreply.github.com>
2023-01-18 16:17:13 -0500

    update
    for loop for lines

--- exercise 1/template-p5-project/js/script.js
+++ exercise 1/template-p5-project/js/script.js
@@ -24,7 +24,7 @@ Description of setup
 
 function setup() {
     createCanvas (500,500);
-    background (100,100,100); 
+    
 
 }
 
@@ -33,5 +33,35 @@ function setup() {
 Description of draw()
 */
 function draw() {
+   
   
-    }
+    let size = width/3 ; 
+  
+    for ( let i = 0 ; i < 3 ; i ++){
+//rect (i*size , 0 , size, size); 
+//rect ( i * size , size , size, size ); 
+//rect ( i * size, size* 2 , size , size ); 
+      
+      line (i  * size , 0 , i * size , 500); 
+      line ( 0 , i * size , 500 , i * size ); 
+}
+
+}
+
+ 
+function mouseClicked () {
+    
+    let size = width / 3 ; 
+
+
+textSize (150);
+text ("X", mouseX, mouseY ); 
+
+ 
+//fill (0); 
+
+
+
+}
+
+    
